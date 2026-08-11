784e390afe9726ba694538381ffd9f37593749b2
israelsegado <israel072003@gmail.com>
2026-08-11 17:15:22 +0200

    manual backup script added

--- 01-backup-auto/backup.sh
+++ 01-backup-auto/backup.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 
-echo hey
+#declaration of the variables with the path
+FILE_ORIGIN="$(pwd)"
+BACKUP_DIR="$FILE_ORIGIN/../backup_registry"
+
+mkdir -p -v "$BACKUP_DIR" #creation of the backup directory
+
+date=$(date +"%Y-%m-%d_%H-%M-%S") #date of the backup
+
+tar -czf "$BACKUP_DIR/backup_$date.tar.gz" "$FILE_ORIGIN" #creation of the backup file
+
+echo "Backup created successfully at "$BACKUP_DIR"/backup_$date.tar.gz"
